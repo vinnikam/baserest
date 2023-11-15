6312d90cd3d3a066217d76881e0503b5193bd580
vinni <vinni9@gmail.com>
2023-11-15 08:06:58 -0500

    Vinni - primer commit

---
+++ src/main/java/co/vinni/funcional/FuncionalApplication.java
@@ -0,0 +1,13 @@
+package co.vinni.funcional;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FuncionalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FuncionalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/co/vinni/funcional/FuncionalApplicationTests.java
@@ -0,0 +1,13 @@
+package co.vinni.funcional;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FuncionalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
